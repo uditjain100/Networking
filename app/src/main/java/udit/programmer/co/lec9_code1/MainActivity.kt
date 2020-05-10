358eae2d1b109ffd3d84e65e31860319e5b3789a
Udit Jain <jain30udit@gmail.com>
2020-05-10 16:16:03 +0530

    Initial commit

---
+++ app/src/main/java/udit/programmer/co/lec9_code1/MainActivity.kt
@@ -0,0 +1,45 @@
+package udit.programmer.co.lec9_code1
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+import kotlin.concurrent.thread
+import kotlin.coroutines.CoroutineContext
+
+class MainActivity() : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val okHttpClient = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("https://api.github.com/users/uditjain100").build()
+        GlobalScope.launch(Dispatchers.Main) {
+            val response = withContext(Dispatchers.IO) {
+                okHttpClient.newCall(request).execute().body!!.string()
+            }
+            val obj = JSONObject(response)
+            val image = obj.getString("avatar_url")
+            val login = obj.getString("login")
+            val name = obj.getString("name")
+            Log.i("Networking", "$login   $name")
+            name_view.text = name
+            login_view.text = login
+            Picasso.get().load(image).into(image_view)
+        }
+
+    }
+}
---
+++ app/src/main/java/udit/programmer/co/lec9_code1/Users.kt
@@ -0,0 +1,10 @@
+package udit.programmer.co.lec9_code1
+
+data class Users(
+    val gistsUrl: String,
+    val reposUrl: String,
+    val followingUrl: String,
+    val receivedEventsUrl: String,
+    val followers: Int,
+    val avatarUrl: String
+)
